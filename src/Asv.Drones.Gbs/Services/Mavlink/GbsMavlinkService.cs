6d6252a5ff21302bb3d3bbd8dbe812cb5cd4af09
Aleksandr Burlakov <u01@asv.me>
2023-08-22 16:59:17 +0300

    feat: add current version number to StatusText log
    Asana: https://app.asana.com/0/1203851531040615/1205255349707810/f

--- src/Asv.Drones.Gbs/Services/Mavlink/GbsMavlinkService.cs
+++ src/Asv.Drones.Gbs/Services/Mavlink/GbsMavlinkService.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
 using System.Reactive.Concurrency;
+using System.Reflection;
 using Asv.Cfg;
 using Asv.Common;
 using Asv.Mavlink;
+using Asv.Mavlink.V2.Common;
 using NLog;
 
 namespace Asv.Drones.Gbs;
@@ -75,6 +77,11 @@ public class GbsMavlinkService : DisposableOnceWithCancel, IGbsMavlinkService
             }, sequenceCalculator,Scheduler.Default, cfg.Server)
             .DisposeItWith(Disposable);
         Server.Start();
+
+        Thread.Sleep(100);
+        
+        var version = Assembly.GetExecutingAssembly().GetVersion().ToString();
+        Server.StatusText.Log(MavSeverity.MavSeverityInfo, version);
     }
 
     public IMavlinkRouter Router { get; }
